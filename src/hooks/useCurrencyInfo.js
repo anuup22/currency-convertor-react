51f133b23a16b8a76552ef81e246fb40c0a85df9
Anup Gupta <84306754+anuup22@users.noreply.github.com>
2024-03-08 22:27:22 +0530

    api changed

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,33 +1,16 @@
-import {useEffect, useState} from "react"
-
-function useCurrencyInfo(currency){
-    const [data, setData] = useState({})
-
-    useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then((res) => res.json())
-        .then((res) => setData(res[currency]))
-        console.log(data);
-    }, [currency])
-    
-    console.log(data);
-    return data
-}
-
-export default useCurrencyInfo;
-/*
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency){
     const [data, setData] = useState({});
     
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`).then((res) => res.json()).then((res) => setData(res[currency]))
-        console.log(data);
-    }, [currency])
-    console.log(data);
-    return data
+        fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
+            .then((res) => res.json())
+            .then((res) => setData(res.rates))
+            .catch((error) => console.error("Error fetching currency data:", error));
+    }, [currency]);
+
+    return data;
 }
 
 export default useCurrencyInfo;
-*/
